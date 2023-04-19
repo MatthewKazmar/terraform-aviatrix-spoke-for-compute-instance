28549ca46a79961b0ddfc25f263487f4311c74d7
Matt Kazmar <mkazmar@aviatrix.com>
2023-04-19 16:03:00 -0500

    first

---
+++ main.tf
@@ -0,0 +1,42 @@
+data "aviatrix_account" "this" {
+  account_name = var.avx_gcp_account_name
+}
+
+module "instance_spoke" {
+  source         = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  version        = "1.5.0"
+  cloud          = "GCP"
+  region         = var.region
+  name           = "${var.name}-spoke"
+  gw_name        = "${var.name}-spoke-gateway"
+  instance_size  = var.aviatrix_spoke_instance_size
+  insane_mode    = var.insane_mode
+  cidr           = local.avx
+  account        = var.avx_gcp_account_name
+  transit_gw     = var.transit_gateway_name
+  network_domain = var.network_domain
+}
+
+resource "google_compute_subnetwork" "instances" {
+  project = data.aviatrix_account.this.gcloud_project_id
+
+  name          = "${var.name}-instances"
+  ip_cidr_range = local.instances
+  region        = var.region
+  network       = module.instance_spoke.vpc.id
+}
+
+module "instances" {
+  source = "github.com/MatthewKazmar/terraform-gcp-vm"
+
+  count = var.number_of_compute_instances
+
+  avx_gcp_account_name = var.avx_gcp_account_name
+  region               = var.region
+  ssh_public_key       = var.public_key
+  name                 = "${var.prefix}-${count.index}"
+  subnetwork_self_link = module.instances.subnet_workload.self_link
+  instance_number      = count.index
+  network_tags         = local.tags
+  machine_type         = var.compute_instance_size
+}
---
+++ variables.tf
@@ -0,0 +1,76 @@
+variable "avx_gcp_account_name" {
+  description = "GCP account as it appears in the controller."
+  type        = string
+}
+
+variable "transit_gateway_name" {
+  description = "Transit Gateway to connect this spoke to."
+  type        = string
+}
+
+variable "insane_mode" {
+  description = "Enable insane mode on this spoke. Be sure to specify an appropriate instance size."
+  type        = bool
+  default     = false
+}
+
+variable "network_domain" {
+  description = "Network domain to associate this spoke with. Optional."
+  type        = string
+  default     = null
+}
+
+variable "name" {
+  description = "Name of the GKE spoke."
+  type        = string
+}
+
+variable "region" {
+  description = "Region to deploy Aviatrix Spoke and GKE."
+  type        = string
+}
+
+variable "cidr" {
+  description = "CIDR for the Spoke Gateway and GKE ranges. Use /23."
+  type        = string
+
+  validation {
+    condition     = split("/", var.cidr)[1] == "23"
+    error_message = "This module needs a /23."
+  }
+}
+
+variable "aviatrix_spoke_instance_size" {
+  description = "Size of the Aviatrix Spoke Gateway."
+  type        = string
+  default     = null
+}
+
+variable "compute_instance_size" {
+  description = "Size of the Compute instance."
+  type        = string
+  default     = "e2-small"
+}
+
+variable "number_of_compute_instances" {
+  description = "Number of instances to deploy."
+}
+
+variable "use_aviatrix_firenet_egress" {
+  description = "Apply the avx_snat_noip tag to nodes for Egress"
+  type        = bool
+  default     = true
+}
+
+variable "public_key" {
+  description = "Public key for the instances."
+  type        = string
+}
+
+locals {
+
+  avx       = cidrsubnet(var.cidr, 1, 0) # 10.0.0.0/22 -> 10.0.0.0/24
+  instances = cidrsubnet(var.cidr, 1, 1) # 10.0.0.0/22 -> 10.0.1.0/26
+
+  tags = var.use_aviatrix_firenet_egress ? ["avx-snat-noip"] : null
+}
