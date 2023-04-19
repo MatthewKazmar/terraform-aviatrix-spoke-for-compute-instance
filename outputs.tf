6fa67bc740f3e83f8a443c3cdc1e65d4b5d7f72d
Matt Kazmar <mkazmar@aviatrix.com>
2023-04-19 16:06:01 -0500

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "instances" {
+  description = "The GCP instance resources"
+  value       = module.instances[*].instance
+}
+
+output "names_ips" {
+  description = "Hash of name/IP"
+  value       = merge(module.instances[*].name_ip)
+}
